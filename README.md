0a0f06ea5dff183c93c7c8e17adacca3c47aafca
Mark <markrussellemail@gmail.com>
2023-01-22 15:41:49 +0000

    add in some macros that use gpio's to enable timing with a logic analyser. Update README.txt. Add doc for Pico pinout.

--- README.md
+++ README.md
@@ -1,31 +1,41 @@
 # picoAudioSampler
-Raspberry pi pico audio sampler synth toy
+Raspberry pi Pico audio sampler synth toy.
+The Pico may not be the best MCU for DSP but i will have a go and see.
 
-Hardware...
-    *) Electret microphone
-    *) Adafruit MAX98357 I2S Class-D Mono Amp 
-        https://learn.adafruit.com/adafruit-max98357-i2s-class-d-mono-amp?view=all
-    *) power supply for the Max98357 
-        at 5V (3W max) it can draw up to 800mA good to have 650mA
-    *) 4 Ohm speaker
-        pimoroni link...
+## Hardware
+1. Pi Pico
+    - (two if you want to use a pi pico-probe for SWD debugging)
+3. Electret microphone,
+    -I had a PCB module with onboard Op-amp
+5. Adafruit MAX98357 I2S Class-D Mono Amp. \
+    - https://learn.adafruit.com/adafruit-max98357-i2s-class-d-mono-amp?view=all
+6. Power supply for the Max98357.
+    -   at 5V (3W max) it can draw up to 800mA good to have 650mA
+7. 4 Ohm speaker, i used one from /pimoroni link.../
+8. Optional, logic analyser
 
-I2S..
-used the pico-playground example but defined the pins
-#define PICO_AUDIO_I2S_DATA_PIN         (9)
-#define PICO_AUDIO_I2S_CLOCK_PIN_BASE   (10)
-// #define l/R ...                      (PICO_AUDIO_I2S_CLOCK_PIN_BASE+1)
+### Circuit simulations use Falstad
 
-Software...
-    *)
-    You should clone it alongside pico-sdk
-        $ ls
-        pico-examples pico-sdk        pico-extras      picotool
-        openocd       pico-playground picoAudioSampler picoprobe
+### Mic pins
 
-    *)
-    How to build OpenOCD etc see /doc
 
-    *)
-    I didn't use this but, here for ref...
+### I2S pins
+Used the pico-playground example so far and defined the pins \
+#define PICO_AUDIO_I2S_DATA_PIN         (9) \
+#define PICO_AUDIO_I2S_CLOCK_PIN_BASE   (10) \
+// #define l/R ...                     (PICO_AUDIO_I2S_CLOCK_PIN_BASE+1)
+
+## Software
+1. You should clone it alongside pico-sdk\
+        $ ls \
+        pico-examples pico-sdk        pico-extras       picotool \
+        openocd       pico-playground picoAudioSampler  picoprobe
+
+1. How to build OpenOCD etc see /doc
+
+1. Create a pi Pcio VSCode project, I didn't use this but, here for ref... \
     https://github.com/raspberrypi/pico-project-generator
+
+1. Logic analyser software: $ pulseview
+    - Use like 12M samples
+
--- main.c
+++ main.c
@@ -22,21 +22,89 @@
    3.3v (pin 36) -> VCC on microphone board
    GND (pin 38)  -> GND on microphone board
 */
-
 #define ADC_NUM     ( 0 )
 #define ADC_PIN     ( 26 + ADC_NUM )
-#define ADC_VREF    ( 3.3 )
 #define ADC_RANGE   ( 1 << 12 )
+#define ADC_VREF    ( 3.3 )
 #define ADC_CONVERT ( ADC_VREF / ( ADC_RANGE - 1 ) )
 
-int main( )
+/************************************************************************************/
+/* Initialise some gpio for debugging time via logic analyser */
+typedef struct
 {
-    const uint led_pin = 25;
+    const uint pin;
+    bool state;
+} logicAnalyserGpioDebugPin_t;
+
+/* Use this as a handle to use the gpio output pins*/
+enum
+{
+    loop_start,
+    blank1,
+    blank2,
+    blank3,
+};
+
+#define log_time_pin_start( x_ )    ( gpio_put( timingpins[x_].pin, true ) )
+#define log_time_pin_stop( x_ )     ( gpio_put( timingpins[x_].pin, false ) )
+
+#define log_time_state_toggle( x_ ) ( timingpins[x_].state = !timingpins[x_].state )
+#define log_time_pin( x_ )          ( gpio_put( timingpins[x_].pin, timingpins[x_].state ) )
+
+
+//.. todo, is there abetter way...?
+#define log_time_pin_toggle( x_ )   ( gpio_put( timingpins[x_].pin, log_time_state_toggle(x_) ) )
+
+//todo doesn't work
+void init_debug_pins( logicAnalyserGpioDebugPin_t *odpins )
+{
+    for( int i = 0; i < sizeof( odpins ) / sizeof( logicAnalyserGpioDebugPin_t );
+         ++i )
+    {
+        gpio_init( odpins[i].pin );
+        gpio_set_dir( odpins[i].pin, GPIO_OUT );
+        gpio_put( odpins[i].pin, false );
+    }
+}
+
+//todo
+/* some wrapper function to time just a function..
+ * time( printf( "Beep boop, listening...\n" ) );
+ * not what i wanted to do and doesn't work if there ia a return'd value
+ */
+#define time( f_, i_ ) log_time_pin_start( i_ ); f_; log_time_pin_stop( i_ );
+
+//todo a proper wrapper if you can remember, look in gtf
+
+
+
+
+/************************************************************************************/
+
+int main( void )
+{
+    logicAnalyserGpioDebugPin_t timingpins[] = { { .pin = 12, .state = false },
+                                                 { .pin = 13, .state = false },
+                                                 { .pin = 14, .state = false },
+                                                 { .pin = 15, .state = false } };
+
+    const uint led_pin                       = 25;
     uint adc_raw;
 
     // Initialize LED pin
     gpio_init( led_pin );
     gpio_set_dir( led_pin, GPIO_OUT );
+
+    // initialise some gpio for debugging time via logic analyser
+    // init_debug_pins( timingpins );
+    for( int dpin = 0;
+         dpin < sizeof( timingpins ) / sizeof( logicAnalyserGpioDebugPin_t );
+         ++dpin )
+    {
+        gpio_init( timingpins[dpin].pin );
+        gpio_set_dir( timingpins[dpin].pin, GPIO_OUT );
+        gpio_put( timingpins[dpin].pin, false );
+    }
 
     stdio_init_all( );
     printf( "Beep boop, listening...\n" );
@@ -49,11 +117,20 @@ int main( )
     adc_gpio_init( ADC_PIN );
     adc_select_input( ADC_NUM );
 
+    // status on
+    gpio_put( led_pin, true );
+
     while( 1 )
     {
-        adc_raw = adc_read( ); // raw voltage from ADC
-        printf( "%.2f\n", adc_raw * ADC_CONVERT );
-        gpio_put( led_pin, true );
+        // log_time_state_toggle( loop_start );
+        // log_time_pin( loop_start );
+        log_time_pin_toggle( loop_start );
+
+        log_time_pin_start( 1 );
+        adc_raw = adc_read( ); // raw voltage from ADC... ~2.5uS
+        log_time_pin_stop( 1 );
+
+        time( printf( "%.2f\n", adc_raw * ADC_CONVERT ), 2 );
         sleep_ms( 10 );
     }
 
